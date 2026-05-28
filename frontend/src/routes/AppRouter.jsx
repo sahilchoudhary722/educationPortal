d706c733259170f3be2806b27293c2a7bac8ded5
Sahil <sahilchoudhary4180@gmail.com>
2026-05-28 13:57:55 +0530

    fix library relative import path

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -11,7 +11,7 @@ import TeacherDashboard from "../pages/TeacherDashboard";
 import Assignments from "../pages/Assignments";
 import AddAssignment from "../pages/AddAssignment";
 import Attendance from "../pages/Attendance";
-import Library from "edumanage-portal/frontend/src/pages/Library.jsx";
+import Library from "../pages/Library.jsx";
 import Store from "../pages/Store";
 import Cart from "../pages/Cart";
 import NotFound from "../pages/NotFound";
